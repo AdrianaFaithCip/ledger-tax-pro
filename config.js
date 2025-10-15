2681ce9f04e5e2def8f7208e0150d9faa8c53478
AdrianaFaithCip <152120513+AdrianaFaithCip@users.noreply.github.com>
2025-10-15 17:23:14 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,12 @@
 <script>
-  // Unique namespace for your app
   window.APP_ID = "ledger-tax-pro";
-
-  // Firebase configuration
   window.FIREBASE_CONFIG = {
-    apiKey: "AIzaSyD2PBBjYxJQ1qryWD4XG8Tqzso8grJOy-yo",
+    apiKey: "…",
     authDomain: "ledger-tax-pro.firebaseapp.com",
     projectId: "ledger-tax-pro",
     storageBucket: "ledger-tax-pro.firebasestorage.app",
-    messagingSenderId: "808408783613",
-    appId: "1:808408783613:web:dedd71ff00776e53763c94",
-    measurementId: "G-SCB01RKT LJ"
+    messagingSenderId: "…",
+    appId: "…"
+    // measurementId is optional; you can omit it.
   };
 </script>
