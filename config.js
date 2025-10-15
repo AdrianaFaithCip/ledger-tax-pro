61559ad9d84f4e0bb16cae5c81d5b24cf886bc9b
AdrianaFaithCip <152120513+AdrianaFaithCip@users.noreply.github.com>
2025-10-15 17:36:33 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
+// config.js  (no <script> tags)
 window.APP_ID = "ledger-tax-pro";
 
 window.FIREBASE_CONFIG = {
-  apiKey: "YOUR_API_KEY",
+  apiKey: "AIzaSyD2PBBjYxJQ1qryWD4XG8Tqzso8grJOy-yo",
   authDomain: "ledger-tax-pro.firebaseapp.com",
   projectId: "ledger-tax-pro",
   storageBucket: "ledger-tax-pro.firebasestorage.app",
   messagingSenderId: "808408783613",
   appId: "1:808408783613:web:dedd71ff00776e53763c94"
- 
+  // measurementId is optional; you can omit it or add it like:
+  // measurementId: "G-XXXXXXX"
 };
-
-
