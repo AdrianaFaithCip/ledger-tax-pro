340be2d6d62c3bf68ba334acdcfcc1aa4f19d992
AdrianaFaithCip <152120513+AdrianaFaithCip@users.noreply.github.com>
2025-10-15 17:13:36 -0400

    Update config.js

---
+++ config.js
@@ -0,0 +1,15 @@
+<script>
+  // Unique namespace for your app
+  window.APP_ID = "ledger-tax-pro";
+
+  // Firebase configuration
+  window.FIREBASE_CONFIG = {
+    apiKey: "AIzaSyD2PBBjYxJQ1qryWD4XG8Tqzso8grJOy-yo",
+    authDomain: "ledger-tax-pro.firebaseapp.com",
+    projectId: "ledger-tax-pro",
+    storageBucket: "ledger-tax-pro.firebasestorage.app",
+    messagingSenderId: "808408783613",
+    appId: "1:808408783613:web:dedd71ff00776e53763c94",
+    measurementId: "G-SCB01RKT LJ"
+  };
+</script>
